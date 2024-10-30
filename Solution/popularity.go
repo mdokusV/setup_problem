4163b5bf532feea8fdf2b7c6c1a2b46abe9e81a7
Mateusz Dokowicz <matdok@st.amu.edu.pl>
2024-10-30 09:21:55 +0000

    initial try with popularity

--- Solution/popularity.go
+++ Solution/popularity.go
@@ -2,13 +2,40 @@ package solution
 
 import (
 	"first/models"
-	"time"
+	"sort"
 )
 
-func popularitySolution(initialState models.InitialState) (models.CMaxValue, time.Duration) {
-	start := time.Now()
-	state := transformInitialState(initialState)
+func PopularitySolution(state State) models.CMaxValue {
+	workers := state.workers
+	tasks := state.tasks
+	setups := state.setups
 
-	elapsed := time.Since(start)
-	return models.CMaxValue(len(state.workers)), elapsed
+	for s := range setups {
+		setup := &setups[s]
+		setup.alphaAverageTasks(1)
+	}
+
+	for t := range tasks {
+		task := &tasks[t]
+		task.alphaAverageSetups(1)
+	}
+
+	sort.Slice(tasks, func(i, j int) bool {
+		return tasks[i].setupPopularity+float64(tasks[i].time) < tasks[j].setupPopularity+float64(tasks[i].time)
+	})
+
+	for t := range tasks {
+		task := &tasks[t]
+		sort.Slice(workers, func(i, j int) bool {
+			return workers[i].predictSum < workers[j].predictSum
+		})
+		minWorker := &workers[0]
+		minWorker.addTaskWithPredict(task)
+	}
+	sort.Slice(workers, func(i, j int) bool {
+		return workers[i].cSum > workers[j].cSum
+	})
+	value := workers[0].cSum
+
+	return models.CMaxValue(value)
 }
